63f4b96ba1b244b004446086cc4fd36736428dd1
Wen Yu <wyu10amy@hotmail.com>
2021-06-02 06:09:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 
-def speak_to_grandma
-
+def speak_to_grandma(phrase = "HUH?! SPEAK UP, SONNY!")
+  puts phrase
 end
 
 # Whatever you say to grandma, she should respond with
